c839aa0644006e1486684b134485e73cee96f52d
Samuel Stark <samuel.winslow.stark@gmail.com>
2026-04-24 22:54:37 +0100

    First shot at grammar

--- grammar.js
+++ grammar.js
@@ -10,8 +10,75 @@
 export default grammar({
   name: "turnip_text",
 
+  extras: ($) => [],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
-  }
+    source_file: ($) => repeat($._group),
+
+    _group: ($) =>
+      choice(
+        $.escaped,
+        $._newline,
+        $.raw_scope,
+        $.eval_bracket,
+        $.comment,
+        $.dash,
+        $.scope,
+        $._anything_else,
+      ),
+
+    raw_scope: ($) =>
+      seq($.raw_scope_open, optional($.raw_scope_internal), $.raw_scope_close),
+    raw_scope_open: ($) => choice(/###\{/, /##\{/, /#\{/),
+    raw_scope_close: ($) => choice(/\}\#\#\#/, /\}\#\#/, /\}\#/),
+    raw_scope_internal: ($) =>
+      repeat1(
+        choice(
+          $.escaped,
+          $._newline,
+          $.eval_bracket_open,
+          $.eval_bracket_close,
+          $.comment,
+          $.dash,
+          $.scope,
+          $._anything_else,
+        ),
+      ),
+
+    eval_bracket: ($) =>
+      seq(
+        $.eval_bracket_open,
+        optional($.eval_bracket_internal),
+        $.eval_bracket_close,
+      ),
+    eval_bracket_open: ($) => choice(/\[---/, /\[--/, /\[-/, /\[/),
+    eval_bracket_close: ($) => choice(/---\]/, /--\]/, /-\]/, /\]/),
+    eval_bracket_internal: ($) =>
+      repeat1(
+        choice(
+          $.escaped,
+          $._newline,
+          $.raw_scope_open,
+          $.raw_scope_close,
+          $.comment,
+          $.dash,
+          $.scope,
+          $._anything_else,
+        ),
+      ),
+
+    // TODO actual block-scope and inline-scope
+    scope: ($) => seq("{", $._group, "}"),
+
+    comment: ($) =>
+      prec(-1, seq("#", optional(/[^\{\r\n][^\r\n]*/), $._newline)),
+
+    escaped: ($) => token(seq("\\", /./)),
+
+    _newline: ($) => choice(/\r\n/, /\r/, /\n/),
+
+    dash: ($) => choice(/-/, /--/, /---/),
+
+    _anything_else: ($) => /[^\r\n\\\#\[\]\{\}\-]+/,
+  },
 });
